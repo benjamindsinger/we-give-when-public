b8f02b7ad23e85710fd2eba971d26843f8d3c562
Alex Soble <asoble@gmail.com>
2017-05-30 07:58:18 -0700

    Add march section

--- client/app/bundles/DemocracySpring/components/DemocracySpringPage.jsx
+++ client/app/bundles/DemocracySpring/components/DemocracySpringPage.jsx
@@ -23,6 +23,9 @@ export default class DemocracySpringPage extends React.Component {
         {this.renderMotivationStatement()}
         {this.renderGiveWhen()}
         {this.renderBenefitStatement()}
+        {this.renderExplanation()}
+        {this.renderEmphasisStatement()}
+        {this.renderVideoSection()}
       </div>
     );
   }
@@ -39,9 +42,8 @@ export default class DemocracySpringPage extends React.Component {
   renderHeadlineSection () {
     return (
       <div>
-        <div className="section headline_section"
-             style={{display: 'flex', backgroundColor: 'black', width: '100%'}}>
-          <div className="content" style={{flex: 1, padding: '30px 60px', height: 360}}>
+        <div style={{display: 'flex', backgroundColor: 'black', width: '100%'}}>
+          <div style={{flex: 1, padding: '30px 60px', height: 360}}>
             <h1 className="headline">
               <span style={{ color: "white" }}>
                 Democrats
@@ -248,6 +250,73 @@ export default class DemocracySpringPage extends React.Component {
           </p>
         </div>
       </div>
+    );
+  }
+
+  renderExplanation () {
+    return (
+      <div style={{
+        padding: 30,
+        width: '60%',
+        margin: '0 auto'
+      }}>
+        <h1 className="subheadline">
+          The Democrats need a shakeup.
+        </h1>
+        <p><span style={{fontWeight: 'bold'}}>The DNC ended its ban on corporate lobbyists attending fundraisers and serving on the party committee.</span> The DNC leadership continues to side with corporate donors and the failed establishment instead of its own progressive voters.</p>
+
+        <p><span style={{fontWeight: 'bold'}}>Every day Tom Perez and DNC don't break with Big Money</span>--by reinstating the ban on corporate lobbyists and supporting reform--<span style={{fontWeight: 'bold'}}>chip in to help hire organizers to shift the Democratic Party to resist Trump, act progressive, and break with big money.</span></p>
+
+        <p>That’s right: every day the party fails to take action, we can get stronger until we have the power to FORCE them to act.</p>
+
+        <p>We’ll lift up those champions in the party who DO stand up for change. And we’ll protest fundraisers and events of candidates and party officials who support the corrupt, failed status quo.</p>
+
+        <div style={{
+          border: '2px solid black',
+          width: '100%',
+          margin: 10
+        }} />
+
+        <h3 className="content_headline">
+          Support this campaign if you think the Democrats must:
+        </h3>
+
+        <ul>
+          <li style={{fontWeight: 'bold'}}>Fiercely resist Trump and his agenda of hate and greed</li>
+          <li style={{fontWeight: 'bold'}}>Embrace an unapologetically progressive agenda</li>
+          <li style={{fontWeight: 'bold'}}>Break with Big Money so they can represent everyday people</li>
+        </ul>
+      </div>
+    );
+  }
+
+  renderEmphasisStatement () {
+    return (
+      <div style={{display: 'flex', backgroundColor: 'yellow', width: '100%'}}>
+        <div style={{flex: 1, padding: '30px 60px', height: 360}}>
+          <h3 className="content_headline">
+            Speak Up. Stand Up. Support the Movement.
+          </h3>
+
+          <div style={{
+            border: '2px solid black',
+            width: '100%',
+            margin: '10px 0'
+          }} />
+
+          <p>We need a true People's Party. Tell Democrats at every level of government that this is the kind of change we need to protect our people and defend our democracy.</p>
+
+          <p>Support the campaign, then share. Let’s build this movement.</p>
+        </div>
+        <img src="/democracy-spring-marching.jpg"
+             style={{flex: 0, height: 420}}/>
+      </div>
+    );
+  }
+
+  renderVideoSection () {
+    return (
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/lIH-tZEPw8k" frameborder="0" allowfullscreen></iframe>
     );
   }
 
