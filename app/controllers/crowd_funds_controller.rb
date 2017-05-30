c178cf4e83a1a8558047f9459c78c4defa4e54ab
Alex Soble <asoble@gmail.com>
2017-05-30 16:22:16 -0700

    Quick & dirty tie different campaigns to pages

--- app/controllers/crowd_funds_controller.rb
+++ app/controllers/crowd_funds_controller.rb
@@ -1,6 +1,11 @@
 class CrowdFundsController < ApplicationController
 
   def index
+    @crowd_funds = CrowdFund.all
+  end
+
+  def show
+    @crowd_fund = CrowdFund.friendly.find(params[:id])
   end
 
 end
