b8158d7c777b55863c94aa8c14cfe891265d4f06
Alex Soble <asoble@gmail.com>
2017-06-01 15:09:08 -0400

    Add crowd fund type to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 crowd_funds = [
   {
     name: 'Democracy Spring',
+    crowd_fund_type: 'COUNTDOWN',
     id: 1
   },
   {
     name: 'Fight for $15',
+    crowd_fund_type: 'SLINGSHOT',
     id: 2
   }
 ]
