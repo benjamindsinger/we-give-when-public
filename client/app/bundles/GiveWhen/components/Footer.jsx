7216a8489dcfdc9bf50abd5a5f6eb1e14be5943a
Alex Soble <asoble@gmail.com>
2017-06-04 17:23:12 -0400

    We're going to need a header and footer

---
+++ client/app/bundles/GiveWhen/components/Footer.jsx
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Footer extends React.Component {
+
+
+};
---
+++ client/app/bundles/GiveWhen/components/Header.jsx
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Header extends React.Component {
+
+
+};
