62516ec8754a3eb1314303f54c6b83b8678d939e
Alex Soble <asoble@gmail.com>
2017-06-05 12:33:21 -0400

    Make stripe form look decent on mobile

--- client/app/bundles/GiveWhen/components/CardDetailsForm.jsx
+++ client/app/bundles/GiveWhen/components/CardDetailsForm.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Money from '../helpers/money.jsx';
+
 export default class CardDetailsForm extends React.Component {
 
   static propTypes = {
     stripePublishableKey: React.PropTypes.string.isRequired,
 
     onType: React.PropTypes.func.isRequired,
-
     selectedAmount: React.PropTypes.number.isRequired,
     selectedMonthlyMaximum: React.PropTypes.number.isRequired,
 
@@ -31,7 +32,8 @@ export default class CardDetailsForm extends React.Component {
 
     const style = {
       base: {
-        fontSize: '18px',
+        fontSize: '22px',
+        lineHeight: '36px'
       },
     };
 
@@ -106,34 +108,61 @@ export default class CardDetailsForm extends React.Component {
 
   renderStripeForm () {
     return (
-      <div>
-        <form id="payment-form">
-          <div className="form-row">
-            <div id="card-element">
-              {/* a Stripe Element will be inserted here.*/}
-            </div>
+      <div className="personal__details__form__wrapper color_scheme__white_blue">
+        <div className="personal__details__form card__details__form">
 
-            {/* Used to display Element errors. */}
-            <div id="card-errors"></div>
+          <div className="summary">
+            <p>Fight back amount: ${Money.renderAmountInCentsAsDollars(this.props.selectedAmountInCents)}
+              <a className="edit" onClick={this.props.onClickEdit}>
+                [edit]
+              </a>
+            </p>
+
+            <p>Monthly maximum amount: ${Money.renderAmountInCentsAsDollars(this.props.selectedMonthlyMaximumInCents)}
+              <a className="edit" onClick={this.props.onClickEdit}>
+                [edit]
+              </a>
+            </p>
           </div>
 
-          <div className="payment-options-group">
-            <div>
-              <input type="checkbox"
-                     id="cover-fees"
-                     checked={this.props.coverFees} />
-              <label htmlFor="cover-fees">
-                Cover platform and payment processing fees.
-              </label>
-            </div>
-          </div>
+          <form id="payment-form">
+            <div style={{
+              backgroundColor: 'white',
+              height: 40,
+              border: 'none',
+              borderRadius: 5,
+              fontFamily: 'Helvetica',
+              fontSize: 20,
+              color: 'black',
+            }}>
+              <div id="card-element" style={{
+                position: 'relative',
+                top: '2px',
+                left: '6px',
+              }}>
+                {/* a Stripe Element will be inserted here.*/}
+              </div>
 
-          <div id="stripe-form-submit-wrapper">
-            <button id="submit-stripe" className="action-button">
-              Submit
+              {/* Used to display Element errors. */}
+              <div id="card-errors"></div>
+            </div>
+
+            <div className="payment-options-group">
+              <div>
+                <input type="checkbox"
+                       checked={this.props.coverFees} />
+                <label htmlFor="cover-fees">
+                  Cover platform and payment processing fees.
+                </label>
+              </div>
+            </div>
+
+            <button id="submit-stripe"
+                    className="action_button_big">
+              submit →
             </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
