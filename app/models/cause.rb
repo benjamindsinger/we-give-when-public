2687f4ad2d53d151aebd7162bbeee6bfedf22ff9
Alex Soble <asoble@gmail.com>
2017-06-07 14:38:19 -0400

    We'll need a way to turn causes into stripe accounts

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,4 +1,12 @@
 class Cause < ActiveRecord::Base
   has_one :legal_entity
   has_many :crowd_funds
+
+  def create_stripe_account
+    acct = Stripe::Account.create({
+      country: "US",
+      type: "custom"
+    })
+  end
+
 end
