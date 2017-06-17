8d8d0ed136f6ded100e4e8c0bca8aca3f011467e
Alex Soble <asoble@gmail.com>
2017-06-17 14:54:44 -0400

    Define after-sign-in paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    return after_sign_in_path_for_super_admin if resource.super_admin?
+
+    return after_sign_in_path_for_cause_admin(resource)
+  end
+
+  private
+
+    def after_sign_in_path_for_cause_admin(resource)
+      crowd_fund = resource.cause.crowd_funds.first
+
+      dashboard_crowd_fund_url(crowd_fund)
+    end
+
+    def after_sign_in_path_for_super_admin
+      dashboard_crowd_fund_url(CrowdFund.first)
+    end
 end
