0b6791fd8b321c2117e4a38445465ee3f0900386
Alex Soble <asoble@gmail.com>
2017-07-03 14:35:41 -0500

    Use func with proper heart styling

--- client/app/bundles/GiveWhen/components/GiveWhenBlocks.jsx
+++ client/app/bundles/GiveWhen/components/GiveWhenBlocks.jsx
@@ -94,10 +94,7 @@ export default class GiveWhenBlocks extends React.Component {
       <div className="give_when__subsection" id="give">
         <h3 style={{marginTop: 10}}>
           Give
-          <svg x="0px" y="0px" viewBox="0 0 100 100">
-            <path d="M74,55.1c-2.5,3-4.9,5.7-24,20.9l0,0l0,0C30.8,60.8,28.5,58.1,26,55.1c-9.2-11-6.7-23.4,0.6-28.6c6.5-4.6,15.7-2.9,23.4,5.9  c7.7-8.7,16.9-10.5,23.4-5.9C80.7,31.7,83.3,44.1,74,55.1z">
-            </path>
-          </svg>
+          {this.renderHeartIcon()}
         </h3>
 
         <div style={{clear: 'both', marginTop: 15}}>
