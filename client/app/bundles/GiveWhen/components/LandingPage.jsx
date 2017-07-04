86c0141d5a12efe2a610b719d5ec8c0f1ad14457
Alex Soble <asoble@gmail.com>
2017-07-04 09:00:35 -0500

    Some prop types for Landing Page are optional

--- client/app/bundles/GiveWhen/components/LandingPage.jsx
+++ client/app/bundles/GiveWhen/components/LandingPage.jsx
@@ -6,15 +6,15 @@ export default class LandingPage extends React.Component {
   static propTypes = {
     foeHeader: PropTypes.string.isRequired,
     foeSubhead: PropTypes.string.isRequired,
-    foeImgUrl: PropTypes.string.isRequired,
-    foeHex: PropTypes.string.isRequired,
+    foeImgUrl: PropTypes.string,
+    foeHex: PropTypes.string,
     friendHeader: PropTypes.string.isRequired,
     friendSubhead: PropTypes.string.isRequired,
-    friendImgUrl: PropTypes.string.isRequired,
-    friendHex: PropTypes.string.isRequired,
+    friendImgUrl: PropTypes.string,
+    friendHex: PropTypes.string,
     callToActionSentence: PropTypes.string.isRequired,
     theoryOfChangeSentence: PropTypes.string.isRequired,
-    disclaimerParagraphs: PropTypes.string.isRequired,
+    disclaimerParagraphs: PropTypes.array.isRequired,
   }
 
   render () {
