301c8d963aba7e9a36b9721836e45aa4a45fadf8
Alex Soble <asoble@gmail.com>
2017-07-04 09:10:11 -0500

    Sketch out landing page sections

--- client/app/bundles/GiveWhen/components/LandingPage.jsx
+++ client/app/bundles/GiveWhen/components/LandingPage.jsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Disclaimer from './sections/Disclaimer.jsx';
+import Footer from './Footer.jsx';
+
 export default class LandingPage extends React.Component {
 
   static propTypes = {
@@ -17,9 +20,27 @@ export default class LandingPage extends React.Component {
     disclaimerParagraphs: PropTypes.array.isRequired,
   }
 
+  foeSectionStyle () {
+    const foeImgUrl = this.props.foeImgUrl;
+    const foeHex = this.props.foeHex;
+
+    if (foeImgUrl) return { backgroundImage: `url(${foeImgUrl})` };
+
+    return { backgroundColor: foeHex };
+  }
+
+  friendSectionStyle () {
+    const friendImgUrl = this.props.friendImgUrl;
+    const friendHex = this.props.friendHex;
+
+    if (friendImgUrl) return { backgroundImage: `url(${friendImgUrl})` };
+
+    return { backgroundColor: friendHex };
+  }
+
   render () {
     return (
-      <div>
+      <div className="new__crowd__fund__page">
         {this.renderFoeSection()}
         {this.renderFriendSection()}
         {this.renderMechanicsSection()}
@@ -30,19 +51,49 @@ export default class LandingPage extends React.Component {
   }
 
   renderFoeSection () {
-    return null;
+    return (
+      <div style={this.foeSectionStyle()}>
+        <div className="section__headline">
+          {this.props.foeHeader}
+        </div>
+        <div className="section__subheadline">
+          {this.props.foeSubhead}
+        </div>
+      </div>
+    );
   }
 
   renderFriendSection () {
-    return null;
+    return (
+      <div style={this.friendSectionStyle()}>
+        <div className="section__headline">
+          {this.props.friendHeader}
+        </div>
+        <div className="section__subheadline">
+          {this.props.friendSubhead}
+        </div>
+      </div>
+    );
   }
 
   renderMechanicsSection () {
-    return null;
+    return (
+      <div>
+        <div className="call__to__action">
+          {this.props.callToActionSentence}
+        </div>
+        <div className="amount__selection">
+          I'll automatically respond with: $2.
+        </div>
+        <div className="theory__of__change__sentence">
+          {this.props.theoryOfChangeSentence}
+        </div>
+      </div>
+    );
   }
 
   renderDisclaimerSection () {
-    return null;
+    return (<Disclaimer paragraphs={this.props.disclaimerParagraphs} />);
   }
 
 }
