9bbdac52523d5503d9e7220f0d87ac8581f7f33b
Alex Soble <asoble@gmail.com>
2017-08-03 11:58:29 -0500

    Add authentication to causes controller

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,7 +1,21 @@
 class CausesController < ApplicationController
 
+  before_action :authenticate
+
   def acceptance
     @cause = Cause.find_by_id(params[:id])
+  end
+
+  private
+
+  def authenticate
+    return true if current_cause_admin.super_admin == true
+
+    correct_cause = current_cause_admin.cause
+
+    if crowd_fund.cause != correct_cause
+      redirect_to root_url, alert: 'That\'s not your cause, don\'t play.'
+    end
   end
 
 end
