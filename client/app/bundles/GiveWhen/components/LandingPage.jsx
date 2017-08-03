7bedad7fe974822512cb8d4d8f72bf3638bb58f6
Alex Soble <asoble@gmail.com>
2017-08-03 10:54:52 -0500

    Make the logo a reasonable max size

--- client/app/bundles/GiveWhen/components/LandingPage.jsx
+++ client/app/bundles/GiveWhen/components/LandingPage.jsx
@@ -176,7 +176,7 @@ export default class LandingPage extends React.Component {
     return (
       <div>
         <div style={{textAlign: 'center', marginTop: 50}}>
-          <img src={this.props.smallLogoImgPath} />
+          <img src={this.props.smallLogoImgPath} style={{maxHeight: 100}} />
         </div>
         <Disclaimer paragraphs={this.props.disclaimerParagraphs} />
       </div>
