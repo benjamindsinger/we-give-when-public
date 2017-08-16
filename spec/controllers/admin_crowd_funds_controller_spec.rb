34862c21b2a39836397778854cb94c78e76c6b9e
Alex Soble <asoble@gmail.com>
2017-08-16 13:38:20 -0500

    Add auth specs for another action

--- spec/controllers/admin_crowd_funds_controller_spec.rb
+++ spec/controllers/admin_crowd_funds_controller_spec.rb
@@ -50,4 +50,46 @@ describe Admin::CrowdFundsController do
 
   end
 
+  describe '#show' do
+    context 'not logged in' do
+      it 'fails' do
+        make_request
+        expect(response.status).to eq 302
+      end
+    end
+
+    context 'not admin' do
+      let(:cause_admin) {
+        CauseAdmin.create!(
+          email: 'test@example.com',
+          password: 'test-password',
+          cause: cause
+        )
+      }
+
+      it 'fails' do
+        sign_in(cause_admin)
+        make_request
+        expect(response.status).to eq 302
+      end
+    end
+
+    context 'admin' do
+      let(:cause_admin) {
+        CauseAdmin.create!(
+          email: 'test@example.com',
+          password: 'test-password',
+          super_admin: true
+        )
+      }
+
+      it 'succeeds' do
+        sign_in(cause_admin)
+        make_request
+        expect(response.status).to eq 200
+      end
+    end
+
+  end
+
 end
