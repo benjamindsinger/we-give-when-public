0e8c09df55581cb2944d689e1b51c8a843ab9466
Alex Soble <asoble@gmail.com>
2017-08-16 12:40:23 -0500

    Add admin crowd funds controller spec

---
+++ spec/controllers/admin_crowd_funds_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe Admin::CrowdFundsController do
+  def make_request
+    request.env['HTTPS'] = 'on'
+    get :index
+  end
+
+  let(:cause) { Cause.create!(name: 'Fight for Rights') }
+
+  describe '#index' do
+    context 'not logged in' do
+      it 'fails' do
+        make_request
+        expect(response.status).to eq 302
+      end
+    end
+
+    context 'not admin' do
+      let(:cause_admin) {
+        CauseAdmin.create!(
+          email: 'test@example.com',
+          password: 'test-password',
+          cause: cause
+        )
+      }
+
+      it 'fails' do
+        sign_in(cause_admin)
+        make_request
+        expect(response.status).to eq 302
+      end
+    end
+
+    context 'admin' do
+      let(:cause_admin) {
+        CauseAdmin.create!(
+          email: 'test@example.com',
+          password: 'test-password',
+          super_admin: true
+        )
+      }
+
+      it 'succeeds' do
+        sign_in(cause_admin)
+        make_request
+        expect(response.status).to eq 200
+      end
+    end
+
+  end
+
+end
