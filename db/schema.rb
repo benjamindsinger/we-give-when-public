a575965933f96ac52085498a1ccb5b7896c29fe4
Alex Soble <asoble@gmail.com>
2017-08-16 13:59:21 -0500

    Remove left over unused field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815225625) do
+ActiveRecord::Schema.define(version: 20170816185848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20170815225625) do
     t.string "slug"
     t.string "name"
     t.json "page_data"
-    t.datetime "goal_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "cause_id"
