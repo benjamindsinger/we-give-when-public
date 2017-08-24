22dbca8a25642952aa6fc82ff55247fff6780066
Alex Soble <asoble@gmail.com>
2017-08-24 13:33:01 -0500

    Mock Stripe

--- spec/models/crowd_fund_spec.rb
+++ spec/models/crowd_fund_spec.rb
@@ -3,12 +3,21 @@ require "rails_helper"
 RSpec.describe CrowdFund, type: :model do
 
   describe "#charge_funders" do
+    class FakeStripeToken
+      def id; end
+    end
+
+    before do
+      allow(Stripe::Token).to receive(:create).and_return(FakeStripeToken.new)
+      allow(Stripe::Charge).to receive(:create).and_return('yaya!')
+    end
+
     let(:cause) {
       Cause.create!(stripe_account_id: 'AAAAAA', name: 'EEEEEEE')
     }
 
     let(:crowd_fund) {
-      CrowdFund.create!(cause: cause)
+      CrowdFund.create!(cause: cause, name: 'UUUUUU')
     }
 
     context "7 funders" do
@@ -30,13 +39,13 @@ RSpec.describe CrowdFund, type: :model do
 
       context "stripe raises no errors" do
         it "charges 7 funders" do
-          expect(crowd_fund.charge_funders(3)).to eq 1
+          expect(crowd_fund.charge_funders(3).count).to eq 7
         end
       end
 
       context "stripe raises errors on the first two" do
         it "charges 5 funders" do
-          expect(crowd_fund.charge_funders(3)).to eq 1
+          expect(crowd_fund.charge_funders(3).count).to eq 7
         end
       end
 
