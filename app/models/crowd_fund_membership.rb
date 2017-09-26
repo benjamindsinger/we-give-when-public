265f36f916937c3633aabceeaacfa3be616bdb82
Alex Soble <asoble@gmail.com>
2017-09-26 11:43:58 -0500

    Don't charge folks unless their status is active

--- app/models/crowd_fund_membership.rb
+++ app/models/crowd_fund_membership.rb
@@ -5,6 +5,8 @@ class CrowdFundMembership < ApplicationRecord
   belongs_to :funder
 
   def charge_member_based_on_triggers(number_of_triggers, api_to_charge)
+    return unless self.status == 'active'
+
     TriggerCharge.new(
       crowd_fund_membership: self,
       api_to_charge: api_to_charge,
