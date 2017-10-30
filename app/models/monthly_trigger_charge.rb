2dc35dadd76401c2ea7d80747974ce07391fdf7b
Alex Soble <asoble@gmail.com>
2017-10-30 11:05:52 -0500

    Set up after_create hook

--- app/models/monthly_trigger_charge.rb
+++ app/models/monthly_trigger_charge.rb
@@ -1,3 +1,8 @@
 class MonthlyTriggerCharge < ApplicationRecord
   belongs_to :crowd_fund
+  after_create :charge_funders
+
+  def charge_funders
+    crowd_fund.charge_funders(self.number_of_triggers, Stripe::Charge, STDOUT)
+  end
 end
