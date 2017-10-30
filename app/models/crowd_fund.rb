97d58864da7a3fb98abed4e1689d63830ffae983
Alex Soble <asoble@gmail.com>
2017-10-30 11:38:23 -0500

    Add a lot more logging

--- app/models/crowd_fund.rb
+++ app/models/crowd_fund.rb
@@ -18,18 +18,40 @@ class CrowdFund < ApplicationRecord
   def charge_funders(number_of_triggers,
                      api_to_charge = Stripe::Charge,
                      log=STDOUT)
+
+    log.write("Preparing to charge #{crowd_fund_memberships.size} funders...")
+    log.write("\n")
+
+    success_count = 0
+    error_count = 0
+
     crowd_fund_memberships.map do |membership|
       begin
+        log.write("Charging someone...")
+        log.write("\n")
+
         if flat_monthly_amount
           membership.charge_member_for_month(api_to_charge)
         else
           membership.charge_member_based_on_triggers(number_of_triggers, api_to_charge)
         end
+
+        success_count += 1
+        log.write("Succeeded!")
+        log.write("\n")
       rescue => error
+        error_count += 1
         log.write("Error charging card! #{error}")
+        log.write("\n")
         false
       end
     end
+
+    log.write("Successfully charged #{success_count} funders.")
+    log.write("Failed to charge #{error_count} funders.")
+    log.write("\n")
+
+    return crowd_fund_memberships
   end
 
   def url
